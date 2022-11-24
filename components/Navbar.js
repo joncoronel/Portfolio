deadd2c2033ba6409ad005651c1e4712e4e623f5
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-11-24 02:30:56 -0800

    navbar hiding and sizing on scroll

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,46 @@
+"use client";
+
 import classes from "./Navbar.module.scss";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [small, setSmall] = useState(false);
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false);
+      } else {
+        // if scroll up show the navbar
+        setShow(true);
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () => {
+        setSmall(window.pageYOffset > 1);
+        controlNavbar();
+      });
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
   return (
-    <header className={classes.header}>
+    <header
+      className={`${classes.header} ${!show ? classes.hidden : ""} ${
+        small ? classes.scrolled : ""
+      }`}
+    >
       <nav className={classes.nav}>
         <div className={classes.container}>
           <div className={classes.logo}>Logo</div>
