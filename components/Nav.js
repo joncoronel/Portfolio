d57f5ea2a9894bdec96baccbd54fec3acfe703ac
joncoronel <59675970+joncoronel@users.noreply.github.com>
2022-11-27 07:19:42 -0800

    new navbar with framer motion links

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,17 @@
 "use client";
 
-import classes from "./Nav.module.scss";
-import Link from "next/link";
+import styles from "./Nav.module.scss";
+
 import { useEffect, useState } from "react";
 import Burger from "./misc/burger";
 
+import { LazyMotion, m } from "framer-motion";
 
+const loadFeatures = () =>
+  import("./misc/features.js").then((res) => res.default);
 
 export default function Navbar() {
+  const [tab, setTab] = useState("about");
   const [small, setSmall] = useState(false);
   const [show, setShow] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
@@ -48,34 +52,50 @@ export default function Navbar() {
     }
   }, [lastScrollY]);
 
+  const navList = ["about", "work", "contact"];
+
   return (
     <>
       <div
         onClick={() => setOpened(!opened)}
-        className={`${classes.overlay} ${opened ? classes.open : ""}`}
+        className={`${styles.overlay} ${opened ? styles.open : ""}`}
       ></div>
       <header
-        className={`${classes.header} ${!show ? classes.hidden : ""} ${
-          small ? classes.scrolled : ""
+        className={`${styles.header} ${!show ? styles.hidden : ""} ${
+          small ? styles.scrolled : ""
         }`}
       >
-        <nav className={`${classes.container} ${opened ? classes.open : ""}`}>
-          <div className={classes.content}>
-            <a className={classes.logo}>Logo</a>
-            <div onClick={() => setOpened(!opened)} className={classes.burger}>
+        <nav className={`${styles.container} ${opened ? styles.open : ""}`}>
+          <div className={styles.content}>
+            <a className={styles.logo}>Logo</a>
+            <div onClick={() => setOpened(!opened)} className={styles.burger}>
               <Burger opened={opened} setOpened={setOpened} />
             </div>
-            <div className={classes.break}></div>
-            <ul className={`${classes.navLinks} ${opened ? classes.open : ""}`}>
-              <li className={classes.link}>
-                <Link href="/">About</Link>
-              </li>
-              <li className={classes.link}>
-                <Link href="/">Work</Link>
-              </li>
-              <li className={classes.link}>
-                <Link href="/">Contact</Link>
-              </li>
+            <div className={styles.break}></div>
+
+            <ul className={`${styles.navLinks} ${opened ? styles.open : ""}`}>
+              <LazyMotion features={loadFeatures}>
+                {navList.map((link) => (
+                  <li
+                    key={link}
+                    onClick={() => {
+                      setTab(link);
+                      setOpened(false);
+                    }}
+                    className={`${styles.link} ${
+                      tab === link ? styles.activeTab : ""
+                    } `}
+                  >
+                    {link}
+                    {tab === link ? (
+                      <m.div
+                        className={styles.underline}
+                        layoutId="underline"
+                      />
+                    ) : null}
+                  </li>
+                ))}
+              </LazyMotion>
             </ul>
           </div>
         </nav>
