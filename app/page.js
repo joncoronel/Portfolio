e6f2e00dcbec3289c12b1370623a3f0c4eddc7e3
joncoronel <59675970+joncoronel@users.noreply.github.com>
2023-03-14 17:37:24 -0700

    added hero page

--- app/page.js
+++ app/page.js
@@ -3,16 +3,15 @@ import classes from "../styles/Home.module.scss";
 import About from "../components/sections/About";
 import Work from "../components/sections/Work";
 import Contact from "../components/sections/Contact";
+import Hero from "../components/sections/Hero";
 
 export default function HomePage() {
   return (
     <div className={classes.spacing}>
-      <h1> Home page</h1>
-      <p>Somne content</p>
+      <Hero />
       <About />
       <Work />
       <Contact />
-      <Space />
     </div>
   );
 }
---
+++ components/sections/Hero.js
@@ -0,0 +1,17 @@
+import styles from "./Hero.module.scss";
+
+export default function Hero() {
+  return (
+    <section id={"hero"} className={styles.container}>
+      <div className={styles.first}>Hi, my name is</div>
+      <div className={styles.name}>Jonathan Coronel</div>
+      <div className={styles.phrase}>I build things some may find useful</div>
+      <div className={styles.desc}>
+        I’m a software engineer specializing in building (and occasionally
+        designing) exceptional digital experiences. Currently, I’m focused on
+        building accessible, human-centered products at Upstatement.
+      </div>
+      <button className={styles.button28}>Contact Me</button>
+    </section>
+  );
+}
