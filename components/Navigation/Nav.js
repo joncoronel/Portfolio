e1e8842da8dec99a2018dd23ddfa4f7707c56071
joncoronel <59675970+joncoronel@users.noreply.github.com>
2023-03-14 19:11:50 -0700

    customized nav after scroll

--- components/Navigation/Nav.js
+++ components/Navigation/Nav.js
@@ -10,6 +10,7 @@ import { LazyMotion, m } from "framer-motion";
 import Navlink from "./navLink";
 import Image from "next/image";
 import Logo from "../../Images/logofinal.svg";
+import throttle from "lodash.throttle";
 
 const loadFeatures = () =>
   import("../misc/features.js").then((res) => res.default);
@@ -20,6 +21,32 @@ export default function Navbar() {
   const [show, setShow] = useState(true);
 
   const [opened, setOpened] = useState(false);
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.pageYOffset > 60) {
+        if (!small) {
+          setSmall(true);
+        }
+      } else {
+        if (small) {
+          setSmall(false);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    controlNavbar();
+
+    const throttledScroll = throttle(controlNavbar, 100);
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", throttledScroll);
+      return () => {
+        window.removeEventListener("scroll", throttledScroll);
+      };
+    }
+  }, [small]);
 
   useEffect(() => {
     if (opened) {
@@ -57,16 +84,14 @@ export default function Navbar() {
         onClick={() => setOpened(!opened)}
         className={`${styles.overlay} ${opened ? styles.open : ""}`}
       ></div>
-      <header
-        className={`${styles.header} ${!show ? styles.hidden : ""} ${
-          small ? styles.scrolled : ""
-        }`}
-      >
+      <header className={`${styles.header} ${!show ? styles.hidden : ""} `}>
         <LazyMotion features={loadFeatures}>
           <m.nav
             animate={opened ? "open" : "closed"}
             variants={variants}
-            className={`${styles.container} ${opened ? styles.open : ""}`}
+            className={`${styles.container} ${opened ? styles.open : ""} ${
+              small ? styles.scrolled : ""
+            }`}
           >
             <div className={styles.content}>
               <a className={styles.logo}>
