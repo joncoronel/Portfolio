b05bf97be10bb042e43d155d1f3cab5aa2953220
joncoronel <59675970+joncoronel@users.noreply.github.com>
2023-04-18 07:37:59 -0700

    chnaged some text again

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
       <div className={styles.phrase}>I build things some may find useful</div>
       <div className={styles.desc}>
         I’m hha software engineer specializing in building (and occasionally
-        designing) exceptional digital experiences. Currently, I’m focused on
+        designgging) exceptional digital experiences. Currently, I’m focused on
         building accessible, human-centered products at Upstatement.
       </div>
       <button className={styles.button28}>Contact Me</button>
